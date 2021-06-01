64b72c15e44060dbdc64bd6ae9b4c729d1885f27
SanglaHector <sanglahector@gmail.com>
2021-06-01 02:31:11 -0300

    1/6 intento pegarle al setbasepath /app/app

--- app/index.php
+++ app/index.php
@@ -27,7 +27,7 @@ use Middlewares\MDWVerificarToken;
 $database = new Database();
 $app = AppFactory::create();
 //$app->setBasePath('/herokucomanda/app');//localhost
-$app->setBasePath('/heroku_comanda/app');//heroku
+$app->setBasePath('/app/app');//heroku
 // Entrar/Salir/Registrarse
 $app->group('/Sing', function (RouteCollectorProxy $group) {
     $group->post('In/empleados', UsuarioController::class . ':singIn');//listo
